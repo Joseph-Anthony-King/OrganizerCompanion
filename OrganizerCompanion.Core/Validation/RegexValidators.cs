f604c6b57180e50afa438434d239d00c347d9edd
joseph-anthony-king <joseph.king.ca@gmail.com>
2025-10-03 16:36:35 -0700

    Add PasswordValidatedAttribute and update regex patterns
    Introduce PasswordValidatedAttribute for password validation,
    extending RegularExpressionAttribute to enforce rules for
    password length and character requirements. Update
    PasswordRegexPattern and UserNameRegexPattern in
    RegexValidators.cs to meet new validation criteria.
    
    Add unit tests in PasswordValidatedAttributeShould to verify
    constructor behavior and validate various password scenarios,
    ensuring correctness of the new attribute.

--- OrganizerCompanion.Core/Validation/RegexValidators.cs
+++ OrganizerCompanion.Core/Validation/RegexValidators.cs
@@ -8,10 +8,10 @@
         internal const string GuidRegexPattern = @"(^([0-9A-Fa-f]{8}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{12})$)";
         /* Password must be from 4 and up through 20 characters with at least 1 upper case letter, 1 lower case letter, 1 numeric character, 
          * and 1 special character of ! @ # $ % ^ & * + = ? - _ . , */
-        internal const string PasswordRegexPattern = @"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=?\-_.,]).{3,20}$";
+        internal const string PasswordRegexPattern = @"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=?\-_.,]).{4,20}$";
         /* User name must be at least 4 characters and can contain alphanumeric characters and special characters of
          * [! @ # $ % ^ & * + = ? - _ . ,] */
-        internal const string UserNameRegexPattern = @"^[a-zA-Z0-9!@#$%^&*+=<>?-_.,].{3,}$";
+        internal const string UserNameRegexPattern = @"^[a-zA-Z0-9!@#$%^&*+=<>?_.,-]{4,}$";
         // Must be a valid url with an http or https protocol
         internal const string UrlRegexPattern = @"^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&%\$#_=]*)?$";
     }
---
+++ UnitTests/OrganizerCompanion.Core.UnitTests/Validation/PasswordValidatedAttributeShould.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OrganizerCompanion.Core.Validation;
+using OrganizerCompanion.Core.Validation.Attributes;
+
+namespace OrganizerCompanion.Core.UnitTests.Validation
+{
+    [TestFixture]
+    internal class PasswordValidatedAttributeShould
+    {
+        [Test]
+        [Category("Validation")]
+        public void Constructor_ShouldSetDefaultErrorMessageAndPattern()
+        {
+            // Arrange & Act
+            var attribute = new PasswordValidatedAttribute();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(attribute.ErrorMessage, Is.EqualTo("The password must be from 4 and up through 20 characters with at least 1 upper case letter, 1 lower case letter, 1 numeric character, and 1 special character of ! @ # $ % ^ & * + = ? - _ . ,"));
+                Assert.That(attribute.Pattern, Is.EqualTo(RegexValidators.PasswordRegexPattern));
+            });
+        }
+
+        [Test]
+        [Category("Validation")]
+        [TestCase("ValidPass1!", true)]
+        [TestCase("Vp1!", true)] // Minimum length
+        [TestCase("ValidPassword123456!", true)] // Maximum length
+        [TestCase("Vp1", false)] // Too short
+        [TestCase("ThisIsAVeryLongPassword1!", false)] // Too long
+        [TestCase("validpass1!", false)] // No uppercase letter
+        [TestCase("VALIDPASS1!", false)] // No lowercase letter
+        [TestCase("ValidPassword!", false)] // No digit
+        [TestCase("ValidPassword1", false)] // No special character
+        [TestCase("", false)] // Empty string
+        [TestCase(null, true)] // Null is considered valid for optional fields
+        public void IsValid_ShouldReturnCorrectValidationResult(object value, bool expected)
+        {
+            // Arrange
+            var attribute = new PasswordValidatedAttribute();
+
+            // Act
+            var isValid = attribute.IsValid(value);
+
+            // Assert
+            Assert.That(isValid, Is.EqualTo(expected));
+        }
+    }
+}
